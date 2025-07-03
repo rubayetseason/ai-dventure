12cc5d75ab7ccf789a194f55e6188f65980ebe8a
rubayetseason <rubayetislamseason5@gmail.com>
2025-07-03 15:04:14 +0600

    loader and not found added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,22 @@
 import CarouselMobile from "@/components/shared/Home/CarouselMobile";
 import Carousel from "@/components/shared/Home/CarouselPC";
+import Loader from "@/components/shared/loaders/Loader";
 import Searchbox from "@/components/shared/others/Searchbox";
 
 export default function Home() {
   return (
-    <div className="relative">
-      <div className="hidden md:block">
-        <Carousel></Carousel>
-      </div>
-      <div className="md:hidden">
-        <CarouselMobile></CarouselMobile>
-      </div>
-      <div className="w-full absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-        <Searchbox></Searchbox>
+    <div>
+      <Loader></Loader>
+      <div className="relative">
+        <div className="hidden md:block">
+          <Carousel></Carousel>
+        </div>
+        <div className="md:hidden">
+          <CarouselMobile></CarouselMobile>
+        </div>
+        <div className="w-full absolute top-[50%] left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+          <Searchbox></Searchbox>
+        </div>
       </div>
     </div>
   );
