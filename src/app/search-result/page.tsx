241ff318826535ee4385edd172a0f17c47f37b21
rubayetseason <rubayetislamseason5@gmail.com>
2025-07-06 14:49:21 +0600

    ai loader added

---
+++ src/app/search-result/page.tsx
@@ -0,0 +1,11 @@
+import AiLoader from "@/components/shared/loaders/AiLoader";
+
+const SearchResultPage = () => {
+  return (
+    <div>
+      <AiLoader></AiLoader>
+    </div>
+  );
+};
+
+export default SearchResultPage;
